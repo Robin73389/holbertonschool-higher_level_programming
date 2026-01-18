3e46efaaf91bd080f129ab511ac4004e1f281da6
Robin73389 <12229@holbertonstudents.com>
2026-01-18 14:40:22 +0100

    Change the structure

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,9 +6,13 @@ import sys
 if __name__ == "__main__":
 
     nb = (len(sys.argv) - 1)
-    mot = "argument:" if nb == 1 else "arguments."
 
-print("{} {}".format(nb, mot))
+if nb == 0:
+    print("0 arguments.")
+elif nb == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(nb))
 
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+for i, nb in enumerate(sys.argv[1:], start=1):
+    print("{}: {}".format(i, nb))
