49b6086e0a92270a91ec1789cca9801f82b66dcf
Robin73389 <12229@holbertonstudents.com>
2026-01-19 19:27:08 +0100

    Display the matrice

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for a in matrix:
+        D_matrix = ""
+        for i in a:
+            D_matrix += str(i) + " "
+        print("{}".format(D_matrix))
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
