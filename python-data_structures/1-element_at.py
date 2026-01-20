a1f1d378eb7248320a5f3c2cccaa98642f0916af
Robin73389 <12229@holbertonstudents.com>
2026-01-20 14:18:57 +0100

    Delete the len and add idx

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,4 +5,9 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     else:
-        return len(my_list[1:5])
+        return my_list[idx]
+    
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
