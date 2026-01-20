8d12a1289ea2d7c02d09ff8f7465dc988fbbf8f1
Robin73389 <12229@holbertonstudents.com>
2026-01-20 22:37:28 +0100

    Add the format string

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,14 +7,3 @@ def print_matrix_integer(matrix=[[]]):
         for i in a:
             D_matrix += str(i) + " "
         print("{:d}".format(D_matrix))
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
