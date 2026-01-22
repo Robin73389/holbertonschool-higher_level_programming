017ef92060658d6cc8ae473d78e16fffdbfa5f90
Robin73389 <12229@holbertonstudents.com>
2026-01-22 18:48:42 +0100

    print the integer matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
 
     for r in matrix:
-        for c in r:
-            print("{:d}".format(c), end=" ")
+        for i, c in enumerate(r):
+            if i != len(r) - 1:
+                print("{:d}".format(c), end=' ')
+            else:
+                print("{:d}".format(c), end='')
         print()
