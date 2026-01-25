a8e28005c53bb0c2fbebb79e7348642b800cdbcd
Robin73389 <12229@holbertonstudents.com>
2026-01-25 18:04:39 +0100

    Correction pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,21 +4,23 @@ Only integers and floats are allowed.
 Floats are cast to integers.
 Raises TypeError on invalid input.
 Returns the addition result."""
+
+
 def add_integer(a, b=98):
     """Return the addition of two integers.
     Floats are cast to integers.
     Raises TypeError on invalid input."""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    
+
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    
+
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
-    
+
     result = a + b
 
     return result
