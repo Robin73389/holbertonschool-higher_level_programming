6086095986492c316d789501420421e023b119b2
Robin73389 <12229@holbertonstudents.com>
2026-01-25 20:06:34 +0100

    Add the documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""Module that contains the say_my_name function."""
 
 
 def say_my_name(first_name, last_name=""):
+    """Prints My name is <first_name> <last_name>.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str, optional): The last name to print.
+            Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name is not a string.
+        TypeError: If last_name is not a string.
+    """
 
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
