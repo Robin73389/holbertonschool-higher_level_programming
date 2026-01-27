e19aea48f0d84bcb87438c99a526625e754848e3
Robin73389 <12229@holbertonstudents.com>
2026-01-27 17:39:45 +0100

    the class return the square the area

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+class Square
+"""
+
+
+class Square:
+    """
+    Docstring for Square if return the square the area
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
