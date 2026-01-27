888a624b1b14ca1b791acbe24d45ba8e0a522146
Robin73389 <12229@holbertonstudents.com>
2026-01-27 17:37:00 +0100

    Add the docstring

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+class Square wich check size
+"""
+
 
 class Square:
+    """
+    Docstring for Square:
+    """
     def __init__(self, size=0):
         self.__size = size
-    
-
         if size is not int(size):
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
         print()
