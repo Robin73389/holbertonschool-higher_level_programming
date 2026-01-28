b51049296e59b8555d0cced9f55934da123d03d0
Robin73389 <12229@holbertonstudents.com>
2026-01-28 13:33:25 +0100

    Add the docstring

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+class Square
+"""
+
+
+class Square:
+    """
+    Docstring for Square if return the square the area
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
