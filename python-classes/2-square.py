2d01b4ce408fb2ec84f581f8e8123e0b65ebb97d
Robin73389 <12229@holbertonstudents.com>
2026-01-28 14:08:50 +0100

    Add new line

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,14 +5,18 @@ class Square wich check size
 
 
 class Square:
+
     """
-    Docstring for Square:
+    Docstring for Square
     """
+
     def __init__(self, size=0):
-        self.__size = size
-        if size is not int(size):
+        self.__sizeof = size
+
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
-        print()
+        return
+    print()
