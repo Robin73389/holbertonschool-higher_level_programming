b8f2a558ddb0b24d51eec10707cb878a959614a9
Robin73389 <12229@holbertonstudents.com>
2026-01-31 17:06:14 +0100

    Function which allows find the best key of a dictionnary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+
+    best_key = None
+
+    for key in a_dictionary:
+        if best_key is None or a_dictionary[key] > a_dictionary[best_key]:
+            best_key = key
+
+    return best_key
