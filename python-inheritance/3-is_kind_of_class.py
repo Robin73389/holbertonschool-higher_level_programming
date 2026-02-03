b503da653e5dece1c0ce847eb2e56443aa032abe
Robin73389 <12229@holbertonstudents.com>
2026-02-03 10:37:21 +0100

    Add docstring

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Function allow if the object is exactly an instance of the specified class
+"""
+
 
 def is_kind_of_class(obj, a_class):
+    """return object"""
     if isinstance(obj, a_class):
         return True
     else:
