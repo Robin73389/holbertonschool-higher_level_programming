7d1e8a95c586540dc0ef3fff120e4f129798dfb0
Robin73389 <12229@holbertonstudents.com>
2026-02-03 10:58:09 +0100

    My function check if value is inter or <= 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """return Exception message"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name}must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} be greater than 0")
