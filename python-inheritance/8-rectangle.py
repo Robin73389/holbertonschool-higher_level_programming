20997c130a8e6703c0e668dc3ff0c5e3bba62e55
Robin73389 <12229@holbertonstudents.com>
2026-02-03 12:57:23 +0100

    Function check integer_validator

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """return Exception message"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name}must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
