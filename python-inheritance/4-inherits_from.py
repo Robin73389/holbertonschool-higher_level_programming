5eeece40bc6f84e0a94becf3dd6bf071769cb822
Robin73389 <12229@holbertonstudents.com>
2026-02-04 10:33:35 +0100

    Add isinstence

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,8 +7,7 @@ of a class that inherited (directly or indirectly) from a specified class.
 
 def inherits_from(obj, a_class):
     """return obj if True"""
-    return type(obj) is not a_class
-    return isinstance(obj,a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
 
 
 a = True
