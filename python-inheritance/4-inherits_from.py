0a531b74d1435f2823208c9c50fa5a52f03eb0cf
Robin73389 <12229@holbertonstudents.com>
2026-02-04 10:10:29 +0100

    Delete the condition if and else for replace by return

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,19 +6,6 @@ of a class that inherited (directly or indirectly) from a specified class.
 
 
 def inherits_from(obj, a_class):
-    """
-    Checks if an object is an instance of a class that inherited
-    (directly or indirectly) from a specified class.
+    """return obj if True"""
 
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-
-    Returns:
-        True if obj is an instance of a subclass of a_class
-        and not an instance of a_class itself, otherwise False.
-    """
-    if type(obj) is not a_class:
-        return True
-    else:
-        return False
+    return type(obj) is not a_class
