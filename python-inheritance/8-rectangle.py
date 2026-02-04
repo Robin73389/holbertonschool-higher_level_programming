00056b0a6dff590b63ab3cc341c410a3affb35bd
Robin73389 <12229@holbertonstudents.com>
2026-02-04 13:39:35 +0100

    I Deepened my docstring for class Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -15,7 +15,8 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """Return Rectangle which return the methode """
+    """Return Rectangle which return the methode
+    which return the width and height"""
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
