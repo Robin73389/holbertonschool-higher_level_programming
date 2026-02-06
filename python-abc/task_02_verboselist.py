afe27b365b891073f7087ef0dc5a41b51904b4c3
Robin73389 <12229@holbertonstudents.com>
2026-02-06 17:52:50 +0100

    Add . the last first line output

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -11,11 +11,11 @@ class VerboseList(list):
     """
     def append(self, item):
         super().append(item)
-        print("Added [{}] to the list".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, item):
         super().extend(item)
-        print("Extended the list with [{}] items".format(len(item)))
+        print("Extended the list with [{}] items.".format(len(item)))
 
     def remove(self, item):
         super().remove(item)
