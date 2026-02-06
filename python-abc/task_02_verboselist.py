f20a1ab8758c04823a4a63df1a26f81d033596a5
Robin73389 <12229@holbertonstudents.com>
2026-02-06 18:08:09 +0100

    Correction pycodestyle

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -10,13 +10,13 @@ class VerboseList(list):
     Docstring for VerboseList
     """
     def append(self, item):
-         print("Added [{}] to the list.".format(item))
-         super().append(item)
-       
+        print("Added [{}] to the list.".format(item))
+        super().append(item)
+
     def extend(self, item):
         print("Extended the list with [{}] items.".format(len(item)))
         super().extend(item)
-    
+
     def remove(self, item):
         print("Removed [{}] from the list.".format(item))
         super().remove(item)
