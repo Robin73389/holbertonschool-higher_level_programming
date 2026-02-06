ab84c0345f2933c885666355a8afc09992e4378e
Robin73389 <12229@holbertonstudents.com>
2026-02-06 17:28:50 +0100

    Add file import file 10

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,33 +2,7 @@
 """Class BaseGeometry"""
 
 
-class BaseGeometry:
-    """return Exception message"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name}must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """return description of Rectangle"""
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -38,4 +12,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-
