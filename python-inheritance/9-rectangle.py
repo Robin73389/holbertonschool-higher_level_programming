b9f8d26630e751c5042e76e67b418761158adfc9
Robin73389 <12229@holbertonstudents.com>
2026-02-06 17:15:09 +0100

    Add import file 7

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Class BaseGeometry"""
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class BaseGeometry:
     """return Exception message"""
@@ -11,7 +13,7 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
