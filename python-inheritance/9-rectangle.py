9bf8bddaf07af5d259b0d498b8fca0eebe10902f
Robin73389 <12229@holbertonstudents.com>
2026-02-06 17:17:40 +0100

    Déléte class Basegeometry

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,19 +4,8 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """return Exception message"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
 class Rectangle(BaseGeometry):
+
     """return description of Rectangle"""
 
     def __init__(self, width, height):
