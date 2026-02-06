76c77c8e7eb5b1644309232e6f73e7e12890679f
Robin73389 <12229@holbertonstudents.com>
2026-02-06 18:20:41 +0100

    Add return

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -10,17 +10,25 @@ class VerboseList(list):
     Docstring for VerboseList
     """
     def append(self, item):
-        print("Added [{}] to the list.".format(item))
-        super().append(item)
+         print("Added [{}] to the list.".format(item))
+         super().append(item)
+       
 
     def extend(self, item):
         print("Extended the list with [{}] items.".format(len(item)))
         super().extend(item)
-
+    
     def remove(self, item):
         print("Removed [{}] from the list.".format(item))
         super().remove(item)
 
     def pop(self, index=-1):
         print("Popped [{:d}] from the list.".format(self[index]))
-        super().pop(index)
+        return super().pop(index)
+
+vl = VerboseList([1, 2, 3])
+vl.append(4)
+vl.extend([5, 6])
+vl.remove(2)
+vl.pop()
+vl.pop(0)
