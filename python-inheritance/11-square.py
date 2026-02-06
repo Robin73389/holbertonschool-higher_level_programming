9676705850093d7d9cae01f66afc38924d529723
Robin73389 <12229@holbertonstudents.com>
2026-02-06 17:30:01 +0100

    Add import file 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,9 +6,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """return the size of rectangle"""
+    """
+    Docstring for Square
+    """
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
