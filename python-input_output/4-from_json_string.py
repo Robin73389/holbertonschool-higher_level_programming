e9c667410269b39fad54d872e5a284b75c685e37
Robin73389 <12229@holbertonstudents.com>
2026-02-09 23:34:44 +0100

    This fonction return an object représente by Json string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring from_json_string
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This fonction return an object représente by Json string
+    """
+
+    return json.loads(my_str)
+
+    print(from_json_string)
