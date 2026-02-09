c00ad994c54bb0448b9d9924aa66e7dea4a87a76
Robin73389 <12229@holbertonstudents.com>
2026-02-09 23:23:33 +0100

    Return JSON object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring JSON
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Fonction return my_obj
+    """
+    return json.dumps(my_obj)
+
+    print(to_json_string)
