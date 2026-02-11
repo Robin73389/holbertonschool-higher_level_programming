c6121d752ea6efff22b86b68fd889d50f73d34b2
Robin73389 <12229@holbertonstudents.com>
2026-02-11 11:11:15 +0100

    Add f.write

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     Fonction serve a  writes an Object to a text file,
     using a JSON representation:
     """
-    with open('my_list.json', 'w') as json_file:
-        json.dump(my_obj, json_file)
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
