e390c041738f0be80e9a3919d0df4d2067e131e7
Robin73389 <12229@holbertonstudents.com>
2026-02-12 16:20:35 +0100

    fonction allows adds all arguments to a Python list,

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Docstring 7-add_item
+"""
+
+
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    temp = load_from_json_file("add_item.json")
+except (FileNotFoundError):
+    temp = []
+
+for i in sys.argv[1:]:
+    temp.append(i)
+
+save_to_json_file(temp, "add_item.json")
