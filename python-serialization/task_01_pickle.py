5e795d7a1ba5e1fe50a4e65bbe54390e694df822
Robin73389 <12229@holbertonstudents.com>
2026-05-27 21:04:48 +0200

    In course

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import pickle
+
+class CustomObject():
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(filename, f)
+    
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb', encoding='utf-8') as file:
+            load = pickle.load(file)
+
+
+
+if __name__ == "__main__":
+    obj = CustomObject(name="John", age=25, is_student=True)
+    print("Original Object:")
+    obj.display()
+
+    # Serialize the object
+    obj.serialize("object.pkl")
+
+    # Deserialize the object into a new instance
+    new_obj = CustomObject.deserialize("object.pkl")
+    print("\nDeserialized Object:")
+    new_obj.display()
