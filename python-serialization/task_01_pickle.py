4528a68702bea629bccc4218528ba51de3bed864
Debian <debian@debian.debian>
2026-05-28 11:04:14 +0200

    I create program who Serialize and deserialize with pickel module

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,27 +1,61 @@
 #!/usr/bin/env python3
+"""Serializz"""
 
 import pickle
 
+
 class CustomObject():
+    """Class CustomObject"""
     def __init__(self, name, age, is_student):
+        """_summary_
+
+        Args:
+            name(str)
+            age (int):
+            is_student (bool):
+        """
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
+        """Method display
+
+            Args:
+                self()
+        """
         print("Name: {}".format(self.name))
         print("Age: {}".format(self.age))
         print("Is Student: {}".format(self.is_student))
 
     def serialize(self, filename):
-        with open(filename, "wb") as f:
-            pickle.dump(filename, f)
-    
+        """Method serialize
+
+            Args:
+                self()
+                filename
+        """
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+
+        except IOError:
+            return None
+
     @classmethod
     def deserialize(cls, filename):
-        with open(filename, 'rb', encoding='utf-8') as file:
-            load = pickle.load(file)
+        """Method serialize
+            Args:
+                cls()
+                filename()
+        """
+        try:
+            with open(filename, 'rb') as file:
+                load = pickle.load(file)
+                return load
 
+        except (IOError, AttributeError):
+            return None
 
 
 if __name__ == "__main__":
