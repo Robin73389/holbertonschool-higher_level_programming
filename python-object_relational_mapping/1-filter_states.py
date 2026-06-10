f17cb6f7fc5df1db5f5b832655746eed141de766
Robin73389 <12229@holbertonstudents.com>
2026-06-10 10:29:20 +0200

    Delete env in shebang

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """The program print the two last line to my database"""
 
 
@@ -8,13 +8,22 @@ import sys
 
 if __name__ == "__main__":
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                           passwd="root",
-                           db="hbtn_0e_0_usa", charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     query_rows = cur.fetchall()
+
     for row in query_rows[-2:]:
         print(row)
+
     cur.close()
     conn.close()
