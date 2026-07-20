31e8d7961fe3a05a23a69d275e6a32757e8bbe71
Robin73389 <12229@holbertonstudents.com>
2026-07-20 16:38:56 +0200

    Delete conditon else if

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,9 +5,6 @@ const char = process.argv[2]
 if (!char) {
   console.error('No argument')
 }
-else if(!process.argv[3]){
-  console.log("Argument found")
-}
 else {
   console.log('Arguments found')
 }
